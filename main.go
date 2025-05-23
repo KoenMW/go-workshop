eabf8e1bb848b0819ec3fff4f93f044b59829bc7
KoenMW <89910070+KoenMW@users.noreply.github.com>
2025-05-23 20:59:45 +0200

    chore: main.go cleanup

--- main.go
+++ main.go
@@ -2,25 +2,9 @@ package main
 
 import (
 	"fmt"
-	"go-workshop-1/csvutil"
-	"go-workshop-1/regression"
 )
 
 func main() {
 	fmt.Println("Hello, Go 1.24.3!")
 
-	weapons, err := csvutil.LoadWeapons("./data/elden_ring_weapon.csv")
-
-	if err != nil {
-		println("some error occured: ", err.Error())
-		return
-	}
-	datapoints, err := regression.CreateDataPoints(weapons, "PhyDF", []string{"Bst"})
-
-	if err != nil {
-		println("some error occured: ", err.Error())
-		return
-	}
-
-	regression.Reg(datapoints, "PhyDF", []string{"Bst"})
 }
